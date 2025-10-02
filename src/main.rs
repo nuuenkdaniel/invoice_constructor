0e85cc27ecd812ba006f1b79d6b5cc54e5219d50
Daniel Lew <lewdaniel2@gmail.com>
2025-10-02 17:17:10 -0400

    FEAT: new func for inputting data into sqlite db

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
 use std::io;
+use std::env;
+use std::fs;
+use std::path::{PathBuf};
+
+use chrono::prelude::*;
+use rusqlite::{params, Connection, Result};
+
+// Expected input db: file_path, date: YYYY-MM-DD time_start: hh:mm, time_end: hh:mm
+fn input_time(db_path: String, time_start: String, time_end: String, date: Option<String>,) {
+    let date = date.unwrap_or(Local::now().date_naive().to_string());
+    let db_path = PathBuf::from(db_path);
+
+    let conn = Connection::open(db_path);
+    con.execute_batch(
+        "CREATE TABLE IF NOT EXISTS tutoring_time (
+            date TEXT,
+            time_start TEXT,
+            time_end TEXT,
+        ",
+        (),
+        
+}
 
 fn request_string() -> String {
     let mut line = String::new();
@@ -8,10 +30,12 @@ fn request_string() -> String {
     line
 }
 
-fn _request_invoice_info() -> (String, String, String, String) {
+fn request_invoice_info() -> (String, String, String, String) {
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
     println!("Provide the senders name: ");
     let senders_name = request_string();
-    
+
     println!("Provide the location name: ");
     let location_name = request_string();
     println!("Provide the location street address: ");
@@ -23,5 +47,6 @@ fn _request_invoice_info() -> (String, String, String, String) {
 }
 
 fn main() {
+    request_invoice_info();
     println!("TO BE IMPLEMENTED");
 }

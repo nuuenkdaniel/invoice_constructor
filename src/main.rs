d6194cf4d3d0aadf245422032f8c7cd30bcd2012
Daniel <lewdaniel2@gmail.com>
2025-10-02 11:40:54 -0400

    chore: Created tex template for tutoring invoice

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,24 @@ fn request_string() -> String {
     let mut line = String::new();
     io::stdin()
         .read_line(&mut line)
-        .expect("Something went wrong");
-    println!("Got {line}");
+        .expect("Something went wrong when reading line");
     line
 }
 
-fn main() {
+fn _request_invoice_info() -> (String, String, String, String) {
     println!("Provide the senders name: ");
-    let _senders_name = request_string();
+    let senders_name = request_string();
     
-    println!("Provide the location: ");
-    let _location = request_string();
+    println!("Provide the location name: ");
+    let location_name = request_string();
+    println!("Provide the location street address: ");
+    let location_street = request_string();
+    println!("Provide the location city, state, and zip: ");
+    let location_city = request_string();
+
+    (senders_name, location_name, location_street, location_city)
+}
+
+fn main() {
+    println!("TO BE IMPLEMENTED");
 }
